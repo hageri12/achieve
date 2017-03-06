a755d1b038b7c1579c338e39be4538a2d09f1f92
yamanishiyuhei <yuhei@yamanishiyuhei-no-MacBook-Air.local>
2017-03-07 00:24:17 +0900

    auauao

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'frozen-earth-96868.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_setting =
+  ActionMailer::Base.smtp_settings =
   {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SNDGRID_USERNAME'],
