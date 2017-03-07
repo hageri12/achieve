ba6a16c904c11098e252bad5dae546b75b43e46e
yamanishiyuhei <yuhei@yamanishiyuhei-no-MacBook-Air.local>
2017-03-07 22:19:57 +0900

    ll

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
     domain: "heroku.com",
-    adress: "smtp.sendgrid.net",
+    address: "smtp.sendgrid.net",
     port: 587,
     authentication: :plain,
     enable_starttls_auto: true
