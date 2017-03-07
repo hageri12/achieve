bb634003517ce50c64aa40198d6ce562d640e048
yamanishiyuhei <yuhei@yamanishiyuhei-no-MacBook-Air.local>
2017-03-07 22:08:22 +0900

    m

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,19 +1,5 @@
 Rails.application.configure do
 
-
-  config.action_mailer.default_url_options = { host: 'herokuapp.com' }
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings =
-  {
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: "heroku.com",
-    adress: "smtp.sendgrid.net",
-    port: 587,
-    authentication: :plain,
-    enable_starttis_auto: true
-  }
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -91,6 +77,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'herokuapp.com' }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings =
+  {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: "heroku.com",
+    adress: "smtp.sendgrid.net",
+    port: 587,
+    authentication: :plain,
+    enable_starttis_auto: true
+  }
 
 
 
