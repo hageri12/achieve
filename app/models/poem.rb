9b0ee9ff0fe57114d21cad09ddab6d3b0af4a319
yamanishiyuhei <yuhei@yamanishiyuhei-no-MacBook-Air.local>
2017-03-19 16:42:27 +0900

    a

---
+++ app/models/poem.rb
@@ -0,0 +1,3 @@
+class Poem < ActiveResource::Base
+  self.site = "https://mighty-beach-57098.herokuapp.com"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -20,5 +20,6 @@ Rails.application.routes.draw do
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
+  resources :poems, only: [:index, :show]
 
 end
